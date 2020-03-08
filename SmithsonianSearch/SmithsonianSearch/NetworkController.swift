e8c8810fff1617dcb59f7a5363fe01ffa324aac8
Dan Morse <djmthedev@gmail.com>
2020-03-08 12:08:18 -0700

    initial

---
+++ SmithsonianSearch/SmithsonianSearch/NetworkController.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkController.swift
+//  SmithsonianSearch
+//
+//  Created by morse on 3/8/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+    static let shared = NetworkController()
+    let baseURL = URL(string: "")
+    let apiKey =
+}
---
+++ SmithsonianSearch/SmithsonianSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmithsonianSearch
+//
+//  Created by morse on 3/8/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
